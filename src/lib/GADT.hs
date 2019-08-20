42f0f1042d8161ee2af996d87134ffc6af16554a
Carsten König <Carsten.Koenig@hotmail.de>
2019-08-20 13:38:45 +0200

    some documentation

--- src/lib/GADT.hs
+++ src/lib/GADT.hs
@@ -12,16 +12,3 @@ module GADT
 import GADT.Internal
 import GADT.Parsing
 
-
-eval :: WrappedExpr -> Maybe (Either Bool Int)
-eval = Just . unwrap (Left . evalExpr) (Right . evalExpr)
-
-
-evalExpr :: Expr res -> res
-evalExpr (IntE i) = i
-evalExpr (AddE a b) = evalExpr a + evalExpr b
-evalExpr (BoolE b) = b
-evalExpr (IsNullE i) = evalExpr i == 0
-evalExpr (IfE b t e)
-  | evalExpr b = evalExpr t
-  | otherwise = evalExpr e
--- src/lib/GADT/Internal.hs
+++ src/lib/GADT/Internal.hs
@@ -1,9 +1,65 @@
 {-# LANGUAGE GADTs, KindSignatures, RankNTypes, FlexibleInstances #-}
+
+{-|
+Module      : GADT.Internal
+Description : definition of the GADT-style domain specific language 
+Copyright   : (c) Carsten König, 2019
+License     : GPL-3
+Maintainer  : Carsten.Koenig@hotmail.de
+Stability   : experimental
+Portability : POSIX
+
+This module defines a simple expression language with
+
+  - 'Int'-Values
+  - 'Bool'-VAlues
+  - addition of two sub-expressions
+  - 'IsNullE' to check if it's subexpression is `0`
+  - an `if ... then ... else ...` conditional
+
+Here we explore the GADT-approach where the expression-type 'Expr'
+is annotated by the type 'evalExpr' will be returning (a Haskell-Type)
+
+This guarantees that we cannot form semantically *invalid* expressions like
+`2 + false` - for example the 'AddE' constructor only accepts sub-expression
+that will return 'Int's.
+
+This makes the 'evalExpr' function much nicer compared to it's cousin 
+'ADT.Internal.eval'.
+
+The downside is that it makes parsing harder. For example what type should
+be returned by a parser `String -> ?`
+
+`parse "5"` would need to be an `Expr Int` but `parse "false"` needs the
+type `Expr Bool` (or some variant with 'Maybe').
+
+We explore an approach where we wrap both (or really any) `Expr a` into
+an *existential*  type 'WrappedExpr' which solves this problem nicely.
+
+Now as types are erased we loose the ability to quickly check during parsing
+if the types match. For example the parser for `if` needs a way to ensure that
+both the `then` and `else` branch where parsed into the same expression-types
+in order to use the 'IfE' constructor.
+So we need a way to pass around information about the used types - which is
+the job of 'ResType' (a singleton representation of 'Int' and 'Bool') and
+the first parameter of the 'Wrap' constructor.
+
+Finally 'wrap' is used as a nice way to lift 'Expr' values into 'WrappedExpr'
+without having to manually tag the 'ResType', by using the 'KnownResType' class.
+
+The 'KnownResType' class might be controversial, as you could achieve the same
+with a simple evaluater that tracks the type by walking along an expression-tree
+but this gives less boilerplate and next to no runtime cost - it's also a nice
+way to constraint the type-argument of 'Expr' to sensible values - hence the
+name.
+-}
 module GADT.Internal
   ( Expr (..)
   , WrappedExpr (..)
   , ResType (..)
   , KnownResType(..)
+  , eval
+  , evalExpr
   , unwrap
   , wrap
   ) where
@@ -17,6 +73,19 @@ data Expr (res :: Type) where
   BoolE :: Bool -> Expr Bool
   IsNullE :: Expr Int -> Expr Bool
   IfE :: Expr Bool -> Expr res -> Expr res -> Expr res
+
+eval :: WrappedExpr -> Maybe (Either Bool Int)
+eval = Just . unwrap (Left . evalExpr) (Right . evalExpr)
+
+
+evalExpr :: Expr res -> res
+evalExpr (IntE i) = i
+evalExpr (AddE a b) = evalExpr a + evalExpr b
+evalExpr (BoolE b) = b
+evalExpr (IsNullE i) = evalExpr i == 0
+evalExpr (IfE b t e)
+  | evalExpr b = evalExpr t
+  | otherwise = evalExpr e
 
 
 instance Show (Expr res) where
